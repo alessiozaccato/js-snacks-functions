6f23f57baac8bdbea83ddf32f269a21e46404bbd
alessiozaccato <alessiozaccato@gmail.com>
2025-01-16 22:05:16 +0100

    SOLVE: exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,27 +1,30 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-let word = 'javascript';
-word = word.split("");
-let numberVocals = 0;
-let finalString = [];
+const word = 'javascript';
+
 
 
 // Dichiara la funzione qui.
 
-function vocals(string1, string2, number) {
+function vocals(string1) {
+
+    let numberVocals = 0;
+
+    let finalString = [];
+
     const arrayVocals = ["a", "e", "i", "o", "u"];
+
     for (k = 0; k < string1.length; k++) {
-        if (string1[k].charAt(0) == arrayVocals[k]) {
-            string2.push(arrayVocals[k]);
-            number++;
+        if (arrayVocals.includes(string1[k].toLowerCase())) {
+            finalString.push(string1[k]);
+            numberVocals++;
         }
     }
-    return string2, number;
+    return numberVocals, finalString;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-vocals(word, numberVocals, finalString);
-console.log(word, numberVocals, finalString)
+console.log(vocals(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
