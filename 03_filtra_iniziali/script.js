5531508f4b500d63d4b1646b794a78c7868a2c6e
alessiozaccato <alessiozaccato@gmail.com>
2025-01-16 16:56:13 +0100

    SOLVE: third exercise

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,12 +1,27 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const newNames = [];
 
 // Dichiara la funzione qui.
+function compareInitials(stringArray, newArray) {
+    let letter = (prompt("inserisci una lettera:", "a")).toUpperCase();
+    for (i = 0; i < stringArray.length; i++) {
+        if (stringArray[i].charAt(0).toUpperCase() == letter) {
+            newArray.push(stringArray[i]);
+        }
+    }
+    //let's see if the array is empty
+    if (newArray.length == 0) {
+        alert(`non ci sono nomi che iniziano per ${letter}`);
+    } else {
+        return newArray;
+    }
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(compareInitials(names, newNames));
 
 
 
